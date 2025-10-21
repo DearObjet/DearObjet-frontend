478c6271701758d2d6c971b942fe7ac8bcc91b79
hyeon <nguswjd02@gmail.com>
2025-10-21 17:29:18 +0900

    feat: nav 하위 메뉴 구현

--- src/components/aside/aside-tab-icon.tsx
+++ src/components/aside/aside-tab-icon.tsx
@@ -1,7 +1,7 @@
 import type { ComponentType, SVGProps } from 'react';
 
 export interface ButtonProps {
-  icon: ComponentType<SVGProps<SVGSVGElement>>;
+  icon?: ComponentType<SVGProps<SVGSVGElement>>; // optional로 변경
   label: string;
   className?: string;
   disabled?: boolean;
@@ -19,18 +19,20 @@ export const Asidetab = ({
     <button
       type="button"
       disabled={disabled}
-      className={`group flex gap-[1.125rem] border-none bg-transparent p-0 text-[#C1C1C1] outline-none focus:text-white focus:outline-none focus:ring-0 active:text-white ${className}`}
+      className={`group flex items-center gap-[1.125rem] border-none bg-transparent p-0 text-[#C1C1C1] outline-none focus:text-white focus:outline-none focus:ring-0 active:text-white ${className}`}
       onClick={onClick}
     >
-      <Icon
-        className={`${
-          disabled
-            ? 'text-gray-400'
-            : 'text-[#C1C1C1] group-focus:text-white group-active:text-white'
-        }`}
-        width={24}
-        height={24}
-      />
+      {Icon && (
+        <Icon
+          className={`${
+            disabled
+              ? 'text-gray-400'
+              : 'text-[#C1C1C1] group-focus:text-white group-active:text-white'
+          }`}
+          width={24}
+          height={24}
+        />
+      )}
       <span>{label}</span>
     </button>
   );
--- src/components/aside/aside.tsx
+++ src/components/aside/aside.tsx
@@ -25,35 +25,159 @@ const Aside: React.FC = () => {
 
       <nav className="mt-[3.375rem] flex flex-col gap-[1.9375rem]">
         <h2 className="text-sm">Manage</h2>
+        <div className="flex flex-col gap-5">
+          <Asidetab
+            icon={MessageCircleMore}
+            label="관리 홈"
+            className={
+              activeMenu.startsWith('/dashboard')
+                ? 'text-white'
+                : 'text-[#C1C1C1]'
+            }
+            onClick={() => handleClick('/dashboard')}
+          />
+          <ul className="ml-14 flex list-disc flex-col gap-2 marker:text-sm marker:text-[#C1C1C1]">
+            <li>
+              <Asidetab
+                label="대쉬보드"
+                className={
+                  activeMenu === '/dashboard/1'
+                    ? 'text-white'
+                    : 'text-[#C1C1C1]'
+                }
+                onClick={() => handleClick('/dashboard/1')}
+              />
+            </li>
+            <li>
+              <Asidetab
+                label="나의 소품샵 관리"
+                className={
+                  activeMenu === '/dashboard/2'
+                    ? 'text-white'
+                    : 'text-[#C1C1C1]'
+                }
+                onClick={() => handleClick('/dashboard/2')}
+              />
+            </li>
+            <li>
+              <Asidetab
+                label="클래스 예약 관리"
+                className={
+                  activeMenu === '/dashboard/3'
+                    ? 'text-white'
+                    : 'text-[#C1C1C1]'
+                }
+                onClick={() => handleClick('/dashboard/3')}
+              />
+            </li>
+          </ul>
+        </div>
+
+        <div className="flex flex-col gap-5">
+          <Asidetab
+            icon={MessageCircleMore}
+            label="입점관리"
+            className={
+              activeMenu.startsWith('/shop') ? 'text-white' : 'text-[#C1C1C1]'
+            }
+            onClick={() => handleClick('/shop')}
+          />
+          <ul className="ml-14 flex list-disc flex-col gap-2 marker:text-sm marker:text-[#C1C1C1]">
+            <li>
+              <Asidetab
+                label="작가 리스트"
+                className={
+                  activeMenu === '/shop/1' ? 'text-white' : 'text-[#C1C1C1]'
+                }
+                onClick={() => handleClick('/shop/1')}
+              />
+            </li>
+            <li>
+              <Asidetab
+                label="품목 및 재고 관리"
+                className={
+                  activeMenu === '/shop/2' ? 'text-white' : 'text-[#C1C1C1]'
+                }
+                onClick={() => handleClick('/shop/2')}
+              />
+            </li>
+            <li>
+              <Asidetab
+                label="계약서 관리"
+                className={
+                  activeMenu === '/shop/3' ? 'text-white' : 'text-[#C1C1C1]'
+                }
+                onClick={() => handleClick('/shop/3')}
+              />
+            </li>
+          </ul>
+        </div>
+
+        <div className="flex flex-col gap-5">
+          <Asidetab
+            icon={MessageCircleMore}
+            label="정산관리"
+            className={
+              activeMenu.startsWith('/settlement')
+                ? 'text-white'
+                : 'text-[#C1C1C1]'
+            }
+            onClick={() => handleClick('/settlement')}
+          />
+          <ul className="ml-14 flex list-disc flex-col gap-2 marker:text-sm marker:text-[#C1C1C1]">
+            <li>
+              <Asidetab
+                label="정산금액 계산"
+                className={
+                  activeMenu === '/settlement/1'
+                    ? 'text-white'
+                    : 'text-[#C1C1C1]'
+                }
+                onClick={() => handleClick('/settlement/1')}
+              />
+            </li>
+            <li>
+              <Asidetab
+                label="세금계산서 발행"
+                className={
+                  activeMenu === '/settlement/2'
+                    ? 'text-white'
+                    : 'text-[#C1C1C1]'
+                }
+                onClick={() => handleClick('/settlement/2')}
+              />
+            </li>
+            <li>
+              <Asidetab
+                label="정산내역"
+                className={
+                  activeMenu === '/settlement/3'
+                    ? 'text-white'
+                    : 'text-[#C1C1C1]'
+                }
+                onClick={() => handleClick('/settlement/3')}
+              />
+            </li>
+          </ul>
+        </div>
+
         <Asidetab
           icon={MessageCircleMore}
-          label="관리 홈"
+          label="메시지"
           className={
-            activeMenu === '/dashboard' ? 'text-white' : 'text-[#C1C1C1]'
-          }
-          onClick={() => handleClick('/dashboard')}
-        />
-        <Asidetab
-          icon={MessageCircleMore}
-          label="입점관리"
-          className={activeMenu === '/shop' ? 'text-white' : 'text-[#C1C1C1]'}
-          onClick={() => handleClick('/shop')}
-        />
-        <Asidetab
-          icon={MessageCircleMore}
-          label="정산관리"
-          className={
-            activeMenu === '/settlement' ? 'text-white' : 'text-[#C1C1C1]'
+            activeMenu.startsWith('/messages') ? 'text-white' : 'text-[#C1C1C1]'
           }
           onClick={() => handleClick('/settlement')}
         />
         <Asidetab
           icon={MessageCircleMore}
-          label="메시지"
+          label="개인정보"
           className={
-            activeMenu === '/message' ? 'text-white' : 'text-[#C1C1C1]'
+            activeMenu.startsWith('/personal-info')
+              ? 'text-white'
+              : 'text-[#C1C1C1]'
           }
-          onClick={() => handleClick('/message')}
+          onClick={() => handleClick('/settlement')}
         />
       </nav>
 
