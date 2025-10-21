a2f7bee844943a4a7115169cf4987f9872b3a985
hyeon <nguswjd02@gmail.com>
2025-10-21 15:11:06 +0900

    add: aside components 파일 추가

---
+++ src/components/aside/aside.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Aside: React.FC = () => {
+  return (
+    <>
+      <aside className="h-screen bg-black">
+        <div className="p-4 text-2xl font-bold text-white">Aside Component</div>
+      </aside>
+    </>
+  );
+};
+
+export default Aside;
