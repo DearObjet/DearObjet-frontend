0e3534e5dfd4c82122ecf42e2cedd5f8ad9b08d6
hyeon <nguswjd02@gmail.com>
2026-04-15 14:20:04 +0900

    feat: 일반회원 마이페이지 내가 작성한 글 레아아웃 구현

--- src/features/my-page/customer/components/myposts.tsx
+++ src/features/my-page/customer/components/myposts.tsx
@@ -1,3 +1,13 @@
 export const MyPosts = () => {
-  return <div>내가 작성한 글</div>;
+  return (
+    <div className="max-h-[69.5625rem] overflow-y-auto [&::-webkit-scrollbar-thumb]:bg-black [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar]:w-[5px]">
+      <div className="grid grid-cols-3">
+        <img
+          src=""
+          alt="사진"
+          className="h-[15.875rem] w-[15.875rem] object-cover"
+        />
+      </div>
+    </div>
+  );
 };
