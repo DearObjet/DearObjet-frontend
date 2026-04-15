505435339ee91615430e7937dab38b0b0205122b
hyeon <nguswjd02@gmail.com>
2026-04-15 14:53:16 +0900

    remove: 마이페이지 파일 구조 변경

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -14,9 +14,9 @@ import {
 import { OAuthCallback } from '../features/auth';
 import { Signup } from '../features/signup';
 import { CustomerNotice, PartnerNotice } from '../features/notice';
-import { MyPage } from '../features/my-page/customer/pages/my-page';
-import { ArtistSettingPage } from '../features/my-page/customer/pages/artist-setting-page';
-import { ShopSettingPage } from '../features/my-page/customer/pages/shop-setting-page';
+import { MyPage } from '../features/my-page/pages/my-page';
+import { ArtistSettingPage } from '../features/my-page/pages/artist-setting-page';
+import { ShopSettingPage } from '../features/my-page/pages/shop-setting-page';
 import { ThemeCustomizer } from '../features/admin/theme';
 import { ShopManagement } from '../features/shop/shop-management/pages/shop-management';
 import { HomePage } from '../pages/home/home-page';
