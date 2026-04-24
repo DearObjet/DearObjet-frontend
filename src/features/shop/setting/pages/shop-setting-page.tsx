29fa161a48807effb34f111a421c08730c8e3fbe
jaeho614 <jaeho614a@gmail.com>
2026-04-24 17:01:40 +0900

    refactor: shop-setting-page.tsx의 최상위 태그 div -> section 으로 수정

--- src/features/shop/setting/pages/shop-setting-page.tsx
+++ src/features/shop/setting/pages/shop-setting-page.tsx
@@ -2,7 +2,7 @@ import { ToggleSwitch } from '../../../../shared/components/ui/toggleswitch';
 
 export const ShopSettingPage = () => {
   return (
-    <div className="flex h-[20.5625rem] w-[42.5625rem] flex-col gap-4 rounded-2xl bg-white pb-[3.125rem] pl-[3.9375rem] pr-[5.3125rem] pt-[2.1875rem]">
+    <section className="flex h-[20.5625rem] w-[42.5625rem] flex-col gap-4 rounded-2xl bg-white pb-[3.125rem] pl-[3.9375rem] pr-[5.3125rem] pt-[2.1875rem]">
       <h3 className="text-xl">알림 설정</h3>
       <div className="flex h-full w-full flex-col gap-[0.625rem] rounded-md bg-gray-200 px-[1.1875rem] pt-[1.625rem]">
         <ToggleSwitch id="" label="정산알림 on/off" />
@@ -11,6 +11,6 @@ export const ShopSettingPage = () => {
         <ToggleSwitch id="" label="카카오톡 알림톡 수신동의" />
         <ToggleSwitch id="" label="E-mail 수신 동의" />
       </div>
-    </div>
+    </section>
   );
 };
