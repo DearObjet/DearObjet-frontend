688e14b443816d010ba9186d10601a4cced6cdbd
hyeon <nguswjd02@gmail.com>
2026-05-06 22:39:02 +0900

    refactor: 입점작가/소품샵 페이지 변경

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -30,8 +30,8 @@ import { Chat } from '../features/chat/pages/chat';
 import { InventoryManagement } from '../features/artist/inventory-management/pages/inventory-mangement';
 import { OutboundManagement } from '../features/artist/outbound-management';
 import { InboundManagement } from '../features/shop/inbound-management';
-import { ShopTenantManagement } from '../features/shop/tenant-management/pages/artist-tenant-management';
-import { ArtistTenantManagement } from '../features/artist/tenant-management/pages/shop-tenant-management';
+import { ArtistTenantManagement } from '../features/shop/tenant-management/pages/artist-tenant-management';
+import { ShopTenantManagement } from '../features/artist/tenant-management/pages/shop-tenant-management';
 
 export const router = createBrowserRouter([
   // 비로그인 전용
