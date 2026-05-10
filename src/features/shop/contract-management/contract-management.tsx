d1cda359f5c30bf604405d98ae6ffa3252d1c318
hyeon <nguswjd02@gmail.com>
2026-05-10 19:00:16 +0900

    feat: 계약서 관리 페이지 레이아웃 구현

--- src/features/shop/contract-management/contract-management.tsx
+++ src/features/shop/contract-management/contract-management.tsx
@@ -1,3 +1,19 @@
 export const ContractManagement = () => {
-  return <div>계약서 관리</div>;
+  return (
+    <div className="flex h-full gap-4">
+      <section className="h-full w-[59.875rem] bg-white">
+        <h3>입점계약서</h3>
+      </section>
+
+      <div className="grid h-full grid-rows-2 gap-4">
+        <section className="w-[25.125rem] bg-white">
+          <h3>계약서 템플릿</h3>
+        </section>
+
+        <section className="w-[25.125rem] bg-white">
+          <h3>완료된 계약</h3>
+        </section>
+      </div>
+    </div>
+  );
 };
