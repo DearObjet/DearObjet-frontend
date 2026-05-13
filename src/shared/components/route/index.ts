8836a73389f06c5d1fad2bcc990b8ec779d3a5a4
jaeho614 <99231850+jaeho614@users.noreply.github.com>
2026-05-13 16:34:23 +0900

    fix: public-only-route.tsx 삭제 및 route에서 제거. OAturhCallback 접근 제한 제거 (#74)

--- src/shared/components/route/index.ts
+++ src/shared/components/route/index.ts
@@ -1,4 +1,3 @@
 export { ProtectedRoute } from './protected-route';
-export { PublicOnlyRoute } from './public-only-route';
 export { RoleGuard } from './role-guard';
 export { TempOrGuestRoute } from './temp-or-guest-route';
