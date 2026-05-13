bf28ef2f6f094c53679e679a0ad199d3684c3cff
hyeon <nguswjd02@gmail.com>
2026-05-13 22:56:34 +0900

    refactor: 회원가입 페이지 중앙 정렬 레이아웃 수정

--- src/features/signup/pages/sign-up.tsx
+++ src/features/signup/pages/sign-up.tsx
@@ -35,23 +35,24 @@ export const Signup = () => {
   const isBusinessUser = userType !== '일반회원';
 
   return (
-    <div className="flex w-screen flex-col items-center gap-4 p-4">
-      <div className="flex w-full max-w-md gap-2">
-        {USER_TYPES.map((type) => (
-          <Button
-            key={type.value}
-            label={type.label}
-            variant={
-              userType === type.value ? 'secondaryDark' : 'secondaryLight'
-            }
-            onClick={() => dispatch(setUserType(type.value))}
-            className="flex-1"
-            type="button"
-          />
-        ))}
+    <div className="flex w-[calc(100%+20px)] flex-col gap-4 p-4 md:-mx-10 md:w-[calc(100%+80px)] xl:-mx-[19.469rem] xl:w-[calc(100%+(19.469rem*2))]">
+      <div className="mx-auto flex w-full max-w-md flex-col items-center gap-4">
+        <div className="flex w-full gap-2">
+          {USER_TYPES.map((type) => (
+            <Button
+              key={type.value}
+              label={type.label}
+              variant={
+                userType === type.value ? 'secondaryDark' : 'secondaryLight'
+              }
+              onClick={() => dispatch(setUserType(type.value))}
+              className="flex-1"
+              type="button"
+            />
+          ))}
+        </div>
+        {isBusinessUser ? <SignupPartner /> : <SignupCustomer />}
       </div>
-
-      {isBusinessUser ? <SignupPartner /> : <SignupCustomer />}
     </div>
   );
 };
