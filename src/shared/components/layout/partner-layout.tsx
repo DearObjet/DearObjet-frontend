3493cd5ed45fac9480634ba4204177075c60f563
jaeho614 <jaeho614a@gmail.com>
2026-05-15 17:59:59 +0900

    fix: shop-management.tsx에 pb-5 추가 및 overflow-y-auto, flex-1 제거

--- src/shared/components/layout/partner-layout.tsx
+++ src/shared/components/layout/partner-layout.tsx
@@ -11,8 +11,8 @@ export const PartnerLayout = () => {
       <div className="flex min-h-0 flex-1 flex-col">
         <PartnerHeader />
 
-        <main className="flex-1 overflow-hidden bg-gray-100">
-          <div className="mx-auto h-full w-full max-w-[120rem] p-5 px-[3.625rem]">
+        <main className="flex-1 overflow-y-auto bg-gray-100">
+          <div className="mx-auto h-full w-full max-w-[120rem] px-[3.625rem] py-5">
             <Outlet />
           </div>
         </main>
